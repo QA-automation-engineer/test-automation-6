ff9c01bad4948525361fda41fcee606e93ffd9ec
Vladimir Trandafilov <trandafilov.vladimir@gmail.com>
2019-12-27 19:33:01 +0200

    feat(utils, pom): added FileDownloader and pdfcompare lib

---
+++ src/main/java/utils/RequestMethod.java
@@ -0,0 +1,33 @@
+package utils;
+
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpOptions;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.methods.HttpTrace;
+
+/**
+ * Created by Vladimir Trandafilov on 27.12.2019.
+ */
+public enum RequestMethod {
+	OPTIONS(new HttpOptions()),
+	GET(new HttpGet()),
+	HEAD(new HttpHead()),
+	POST(new HttpPost()),
+	PUT(new HttpPut()),
+	DELETE(new HttpDelete()),
+	TRACE(new HttpTrace());
+
+	private final HttpRequestBase requestMethod;
+
+	RequestMethod(HttpRequestBase requestMethod) {
+		this.requestMethod = requestMethod;
+	}
+
+	public HttpRequestBase getRequestMethod() {
+		return this.requestMethod;
+	}
+}
